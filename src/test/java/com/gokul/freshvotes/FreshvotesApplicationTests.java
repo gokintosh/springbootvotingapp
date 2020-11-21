455e773af0bdb5649e282085f828ddd792c83389
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-11-21 19:25:03 +0100

    initialized project

---
+++ src/test/java/com/gokul/freshvotes/FreshvotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.freshvotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshvotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
