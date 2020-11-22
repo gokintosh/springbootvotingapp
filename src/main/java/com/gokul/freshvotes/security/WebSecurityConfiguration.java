51480bcc85c93cb1206016ea031f5adadd18a14a
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-11-22 14:36:41 +0100

    complete login enable csrf

--- src/main/java/com/gokul/freshvotes/security/WebSecurityConfiguration.java
+++ src/main/java/com/gokul/freshvotes/security/WebSecurityConfiguration.java
@@ -1,24 +1,37 @@
 package com.gokul.freshvotes.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("gokul").password("gokul").roles("USER");
-
+        auth.inMemoryAuthentication()
+        .passwordEncoder(getPasswordEncoder())
+        .withUser("gokul").password(getPasswordEncoder().encode("gokul")).roles("USER");
+        
 
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/").permitAll().anyRequest().hasRole("USER").and()
+        http.authorizeRequests().antMatchers("/").permitAll().anyRequest().hasRole("USER").and()
                 .formLogin().loginPage("/login")
+                .defaultSuccessUrl("/dashboard")
                 .permitAll()
                 .and()
                 .logout().logoutUrl("/logout").permitAll();
